4d15ce2837fa06bb10213b3adff2ae9d68d99b0d
cyro.costa <cyro.costa@ambevtech.com.br>
2021-10-16 14:46:21 -0300

    Lidando com eventos e useState

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import './App.css'
-import SayMyName from './Components/SayMyName'
-import Pessoa from './Components/Pessoa'
-import Frase from './Components/Frase'
-import List from "./Components/List"
+import Evento from './Components/Eventos';
+import Form from './Components/Form';
 
-
-function App() { 
-  
-  const nome = "Maria"
+function App() {  
 
   return (
     <div className="App">
-      <h1>Testando Css</h1>
-      <Frase/>  
-      <Frase/>    
-      <SayMyName nome="Cyro"/>
-      <SayMyName nome="Caio"/>
-      <SayMyName nome={nome}/>
-      <Pessoa 
-      nome="Cyro" 
-      idade="25" 
-      profissao="Dev" 
-      foto="https://via.placeholder.com/150"/>
-      <List/>      
+      <h1>Testando Eventos</h1>
+      <Evento numero={1}/>
+      <Evento numero={2}/>
+      <Form/>
+       
     </div>
   );
 }
